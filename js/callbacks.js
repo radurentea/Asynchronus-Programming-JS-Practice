3719c5d5cd43f9eec8f7ad63ece1b57e00442d8f
Radu Rentea <radu.rentea@gmail.com>
2019-05-15 06:45:47 +0300

    Button adds the astronauts to the page

--- js/callbacks.js
+++ js/callbacks.js
@@ -4,7 +4,7 @@ const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
 // Make an AJAX request
-function getJSON(url)
+function getJSON(url, callback)
 {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', url);
@@ -13,7 +13,7 @@ function getJSON(url)
     if (xhr.status === 200)
     {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
@@ -32,4 +32,13 @@ function generateHTML(data)
   `;
 }
 
-btn.addEventListener('click', () => getJSON(astrosUrl));
+btn.addEventListener('click', () =>
+{
+  getJSON(astrosUrl, (json) =>
+  {
+    json.people.map(person =>
+    {
+      getJSON(wikiUrl + person.name, generateHTML);
+    });
+  });
+});
