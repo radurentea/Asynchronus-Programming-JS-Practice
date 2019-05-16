268c6801ebfc974e23e31656608c051d03a104e7
Radu Rentea <radu.rentea@gmail.com>
2019-05-16 07:32:48 +0300

    Use fetch instead of XHR for the API

--- js/promises.js
+++ js/promises.js
@@ -3,36 +3,43 @@ const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const peopleList = document.getElementById('people');
 const btn = document.querySelector('button');
 
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
+function getProfiles(json)
+{
+  const profiles = json.people.map(person =>
+  {
+    return fetch(wikiUrl + person.name)
+      .then(response => response.json());
+  });
+  return Promise.all(profiles);
 }
 
-function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+function generateHTML(data)
+{
+  data.map(person =>
+  {
+    const section = document.createElement('section');
+    peopleList.appendChild(section);
+    section.innerHTML = `
+    <img src=${person.thumbnail.source}>
+    <h2>${person.title}</h2>
+    <p>${person.description}</p>
+    <p>${person.extract}</p>
+    `;
+  });
 }
 
-function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
-    <img src=${data.thumbnail.source}>
-    <h2>${data.title}</h2>
-    <p>${data.description}</p>
-    <p>${data.extract}</p>
-  `;
-}
+btn.addEventListener('click', (event) =>
+{
+  event.target.textContent = "Loading...";
 
-btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+  fetch(astrosUrl)
+    .then(response => response.json())
+    .then(getProfiles)
+    .then(generateHTML)
+    .catch(err =>
+    {
+      peopleList.innerHTML = '<h3>Something went wrong!</h3>';
+      console.log(err);
+    })
+    .finally(() => event.target.remove())
 });
